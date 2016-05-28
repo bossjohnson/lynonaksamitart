96a1afcb80cbe72ab042316940b897b0df09226b
Josh Newsom <newsommeister@gmail.com>
2016-05-28 15:19:24 -0600

    Background becomes opaque when choosing image

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,31 +1,46 @@
-$('.thumbnail').on('mouseenter', function() {
-    $(this).animate({
-        top: '-=10px',
-        height: '+=20px',
-        width: '+=20px'
-    }, 100);
+$(function() {
+
+    randomImage();
+    $('#imgPlacement').hide();
+
+    $('.thumbnail').on('mouseenter', function() {
+        $(this).animate({
+            top: '-=10px',
+            height: '+=20px',
+            width: '+=20px'
+        }, 100);
+    });
+
+    $('.thumbnail').on('mouseleave', function() {
+        $(this).animate({
+            top: '+=10px',
+            height: '-=20px',
+            width: '-=20px'
+        }, 100);
+    });
+
+    $('.thumbnail').on('click', function() {
+        $('header, footer, main').css('opacity', '.2');
+        $('.current').attr('src', $(this).attr('src'));
+        $('#imgPlacement').show();
+
+    });
+
+    $('.current').on('click', function() {
+        $('#imgPlacement').hide();
+        $('header, footer, main').css('opacity', '1');
+    });
+
+    window.setInterval(randomImage, 7000);
 });
 
-$('.thumbnail').on('mouseleave', function() {
-    $(this).animate({
-        top: '+=10px',
-        height: '-=20px',
-        width: '-=20px'
-    }, 100);
-});
+// ================
+// Helper Functions
+// ================
 
-$('.thumbnail').on('click', function() {
-    $('.current').attr('src', $(this).attr('src'));
-    $('.current').show();
-});
-
-$('.current').on('click', function() {
-    $(this).hide();
-});
-
-window.setInterval(function() {
+function randomImage() {
     var randNum = Math.floor(Math.random() * $('.thumbnail').length);
     var url = $('.thumbnail')[randNum].src;
     console.log(url);
     $('main').css('background-image', 'url(' + url + ')');
-}, 5000);
+}
