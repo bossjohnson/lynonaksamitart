9dbdc690eaeaf0ab14ad3bbf8b582985809bac17
Josh Newsom <newsommeister@gmail.com>
2016-06-21 11:47:35 -0600

    Users can click to change image on homepage

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,8 @@
 $(function() {
+    var images = JSON.parse($('#images').val());
+    var currentImageIndex = 0;
     var catShow = false;
+
     var all = $('<a class="category">All</a>');
     var ink = $('<a class="category">Ink</a>');
     var paintings = $('<a class="category">Paintings</a>');
@@ -10,10 +13,30 @@ $(function() {
         .append(paintings)
         .append(commissioned);
 
+    // Previous and next button functionality
+    $('#next').on('click', function() {
+        currentImageIndex =
+            currentImageIndex < images.length - 1 ?
+            ++currentImageIndex :
+            0;
+        $('main').css('background-image', 'url(' +
+                images[currentImageIndex].url.split(' ').join('%20') +
+            ')');
+    });
+    $('#prev').on('click', function() {
+        currentImageIndex =
+            currentImageIndex > 0 ?
+            --currentImageIndex :
+            images.length - 1;
+        $('main').css('background-image', 'url(' +
+                images[currentImageIndex].url.split(' ').join('%20') +
+            ')');
+    });
+
     $('#gallery').on('click', function() {
         !catShow ? $(this).after(categories) : categories.remove();
         catShow = !catShow;
     });
 
-    // $('main').css('background-image', latest.url);
+
 });
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,6 @@ var pg = require('pg');
 var client = new pg.Client('postgres://localhost:5432/lynonsart');
 client.connect();
 
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     getImages().then(function(images) {
@@ -14,7 +12,8 @@ router.get('/', function(req, res, next) {
             return b.upload_date - a.upload_date;
         });
         res.render('index', {
-            latestUrl: images[0].url.split(' ').join('%20')
+            latestUrl: images[0].url.split(' ').join('%20'),
+            images: JSON.stringify(images)
         });
     });
 });
@@ -24,11 +23,11 @@ module.exports = router;
 function getImages() {
     return new Promise(function(resolve, reject) {
         client.query("SELECT * FROM arts", function(err, data) {
-            var arts = [];
+            var images = [];
             for (var i = 0; i < data.rows.length; i++) {
-                arts.push(data.rows[i]);
+                images.push(data.rows[i]);
             }
-            resolve(arts);
+            resolve(images);
         });
     });
 }
