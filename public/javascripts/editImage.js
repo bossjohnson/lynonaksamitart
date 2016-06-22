9b4760e215688832e16618e74666dc16f7424cfb
Josh Newsom <newsommeister@gmail.com>
2016-06-21 21:38:54 -0600

    Added categories into each image container

--- public/javascripts/editImage.js
+++ public/javascripts/editImage.js
@@ -3,11 +3,21 @@ $(function() {
 
     // Edit button functionality
     $('.edit').on('click', function() {
+        var header = $(this).parent();
+        var container = $(this).parent().parent();
+
+        var imageId = container.find('.imageId').val();
+        var imageTitle = container.find('.imageTitle').val();
+        var imageCategory = container.find('.imageCategory').val();
+        var imageDesc = container.find('.imageDesc').val();
 
         $(this).hide();
         $(this).siblings('.save').show();
-        $(this).parent().prop('editing', true);
-        $(this).parent().parent().find('.showImageInfo').show();
+        header.prop('editing', true);
+        container.find('.showImageInfo').show();
+        container.find('.artTitle').val(imageTitle);
+        container.find('.artCat').val(imageCategory);
+        container.find('.description').val(imageDesc);
         $(this).parent().parent().find('input').focus();
         // var image_id = $(this).data().imageId;
         // var xhr = new XMLHttpRequest();
@@ -40,17 +50,17 @@ $(function() {
     // Show edit (or save and delete on mouseover
     $('.imageContainer').on('mouseenter', function() {
         if ($(this).find('.imageHeader').prop('editing')) {
-          $(this).find('.delete, .save').show();
+            $(this).find('.delete, .save').show();
         } else {
-          $(this).find('.delete, .edit').show();
+            $(this).find('.delete, .edit').show();
         }
         $(this).css('box-shadow', '2px 2px 15px black');
     });
     $('.imageContainer').on('mouseleave', function() {
-      if (!$(this).find('.imageHeader').prop('editing')) {
-        $(this).find('.delete, .edit, .save').hide();
-        $(this).css('box-shadow', 'none');
-      }
+        if (!$(this).find('.imageHeader').prop('editing')) {
+            $(this).find('.delete, .edit, .save').hide();
+            $(this).css('box-shadow', 'none');
+        }
     });
 
     // Click animation
@@ -58,13 +68,13 @@ $(function() {
         $(this).animate({
             'top': '+=1px',
             'left': '+=1px'
-        }, 100);
+        }, 50);
     });
     $('.delete, .edit').on('mouseup', function() {
         $(this).animate({
             'top': '-=1px',
             'left': '-=1px'
-        }, 100);
+        }, 50);
     });
 
 });
